544f9f104d60950541ff8dbb8bc5c88510eea472
atacankullabci <atacankullabci@gmail.com>
2020-01-19 23:03:32 +0300

    Show server static data on angular

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,37 @@ const express = require('express');
 
 const app = express();
 
+// app.use((request, response, next) => {
+//   console.log('First Middleware');
+//   // When next function is called the next app.use function is continued (Comment out and check)
+//   next();
+// });
+
 app.use((request, response, next) => {
-  console.log('First Middleware');
-  // When next function is called the next app.use function is continued (Comment out and check)
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers, Origin, X-Requested-With,Content-Type,Accept");
+  response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, OPTIONS");
   next();
 });
 
-app.use((request, response, next) => {
-  response.send('Hello from express !');
+app.use("/api/posts", (request, response, next) => {
+  const posts = [
+    {
+      id: 'sadf12dasf',
+      title: 'First server side post',
+      content: 'This is coming from the backend server'
+    },
+    {
+      id: '123sa2q3qdfs',
+      title: 'Second server side post',
+      content: 'This is coming from the backend server !!'
+    }
+
+  ];
+  response.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,13 +1,22 @@
 import {Post} from './post.model';
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {
+  }
+
   getPosts() {
+    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
     return [...this.posts];
   }
 
@@ -16,7 +25,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title, content};
+    const post: Post = {id: null, title, content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
