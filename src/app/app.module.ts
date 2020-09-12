af0543bce9d85558c4c922fed41e6a21e875e174
atacankullabci <atacankullabi@gmail.com>
2020-09-12 18:12:50 +0300

    Adding node js to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {MatButtonModule, MatCardModule, MatExpansionModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {HeaderComponent} from './header/header.component';
 import {PostListComponent} from './posts/post-list/post-list.component';
 import {HttpClientModule} from '@angular/common/http';
