22a1e17f639349416540d5e23b21f68a72cc4e24
atacankullabci <atacankullabi@gmail.com>
2020-09-21 16:28:36 +0300

    CRUD ops

--- backend/models/post.js
+++ backend/models/post.js
@@ -6,4 +6,8 @@ const postSchema = mongoose.Schema({
   content: {type: String, required: true}
 });
 
-module.exports = mongoose.model('Post', postSchema, 'posts');
+// If you do not provide a collection name mongoose will automatically
+// provide a collection name for you with its model name's plural form
+// You can provide a collection name by yourself with the line below
+//module.exports = mongoose.model('Post', postSchema, 'posts');
+module.exports = mongoose.model('Post', postSchema);
